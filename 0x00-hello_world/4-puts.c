5dc20c829e08cf4aa89c1dbf0f2f1f5f79cb9832
Adham <adhamelsa8er@gmail.com>
2023-08-10 15:59:49 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Description: program to output with puts
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
