2348e12beb7bfbe0b8e0634ba9558f9faaf072b7
Adham <adhamelsa8er@gmail.com>
2023-08-13 16:18:55 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: program to print all numbers from 0 to 9
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: program to print all combinations of single digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar('0' + c);
+		putchar(',');
+		putchar(' ');
+		c++;
+	}
+	return (0);
+}
