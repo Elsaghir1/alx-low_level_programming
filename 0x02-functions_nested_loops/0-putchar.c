110f9fb21808204df8b5ece76982111d6993b8dd
Adham <adhamelsa8er@gmail.com>
2023-08-15 15:42:39 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main"
+
+/**
+ * main - Entry point
+ * Description: program to print all numbers of base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	str[] = "_putchar";
+
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
