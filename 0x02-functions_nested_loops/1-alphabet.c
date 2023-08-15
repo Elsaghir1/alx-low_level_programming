8b071f1802862360f34055ad5d7e76be41b4f9bd
Adham <adhamelsa8er@gmail.com>
2023-08-15 15:52:19 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * main - Entry point
+ * Description: program to print all numbers of base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
