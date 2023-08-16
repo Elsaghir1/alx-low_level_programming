fc3215ffdf7e4ba825db14b3977bef1bf823893f
Adham <adhamelsa8er@gmail.com>
2023-08-15 18:24:23 -0700

    a

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,16 +3,16 @@
 /**
  * main - Entry piont
  * Description: calculate the sum of the multiples of 3 and 5 below 1024
- * Retrun: always 0 (success)
+ * Retrun: Always 0 (Success)
  */
 
-int main(void)
+void main(void)
 {
 	int sum, i;
 
 	for (i = 1; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 	}
 	printf("%d\n", sum);
