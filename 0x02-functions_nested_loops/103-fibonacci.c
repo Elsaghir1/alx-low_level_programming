89fbafc21882cf77703470f96287b48439c03e02
Adham <adhamelsa8er@gmail.com>
2023-08-15 18:41:18 -0700

    a

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry piont
+ * Description: print sum of even fibonacci numbers below 4000000
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum, ans = 0;
+
+	while (true)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if (sum % 2 == 0)
+			ans += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%lu\n", ans);
+	return (0);
+}
