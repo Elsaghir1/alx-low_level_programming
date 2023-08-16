af69e27fff14ed9dd79e3b19b6c848f991db58bb
Adham <adhamelsa8er@gmail.com>
2023-08-15 17:28:56 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - calculate the summation of two numbers
+ * @a: first input
+ * @b: second input
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int num, mul, prod;
 
-	for (num = 0; num <= 9; num++
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 		for (mul = 1; mul <= 9; mul++)
