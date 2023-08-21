9d423bef989a724ec6bb4f47017fd804a8c71ce9
Adham <adhamelsa8er@gmail.com>
2023-08-21 14:56:01 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,8 @@
 
 int _atoi(char *s)
 {
-	int i = 0, ans = 0, len = 0, neg = 1;
+	int i = 0, len = 0, neg = 1;
+	unsigned int ans = 0;
 
 	while (s[len] != '\0')
 		len++;
@@ -20,6 +21,8 @@ int _atoi(char *s)
 			neg *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 			ans = (ans * 10) + (s[i] - '0');
+		else if (ans > 0)
+			break;
 	}
 	return (ans * neg);
 }
