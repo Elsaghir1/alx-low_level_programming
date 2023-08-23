edb35c15af3db03ee5d6234ff824646b1661e585
Adham <adhamelsa8er@gmail.com>
2023-08-23 08:36:55 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int ans = 0;
+
 	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
-			return ((((int)*s1 - 48) - ((int)*s2 - 48)));
+			ans = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
 		}
 		s1++, s2++;
 	}
+	return (ans);
 }
