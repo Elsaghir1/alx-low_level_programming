9073d6287ba4e9059223f67f84b17a67a47b36b3
Adham <adhamelsa8er@gmail.com>
2023-09-01 15:39:39 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function to print the file name
+ *
+ * @argc: the number of arguments in command-line
+ * @argv: vector of string stored the arguments in command-line
+ *
+ * Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
