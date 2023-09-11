8b61001e6110a5ad20dbdbecae9b317e99b9e947
Adham <adhamelsa8er@gmail.com>
2023-09-11 00:27:47 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
