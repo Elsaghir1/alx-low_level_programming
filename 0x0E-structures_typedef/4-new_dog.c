dc3694fbd13a6c31c0b2c36c8a48f7cf8e6ab0a6
Adham <adhamelsa8er@gmail.com>
2023-09-11 03:17:16 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include "dog.h"
 
 /**
+ * _strlen - return the length of string
+ *
+ * @s: input from another function
+ *
+ * Return: Always 0
+*/
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * _strlen - return the length of string
+ *
+ * @s: input from another function
+ *
+ * Return: Always 0
+*/
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
  * *new_dog -  function that creates a new dog.
  *
  * @name: first input
