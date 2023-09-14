a64a37af325fd7cbb39dd82dbdc688ea3ba6b233
Adham <adhamelsa8er@gmail.com>
2023-09-14 02:59:11 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int i = n;
 	va_list v;
 
-	if (n)
+	if (!n)
 	{
 		printf("\n");
 		return;
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	va_start(v, n);
 	while (i--)
-		printf("%s%s", (str = va_arg(v, char *)) ? str : "(nil)",
+		printf("%s%s", (str == va_arg(v, char *)) ? str : "(nil)",
 			i ? (separator ? separator : "") : "\n";
 	va_end(v);
 }
