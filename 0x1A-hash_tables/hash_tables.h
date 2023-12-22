43aafa4cf732a0755f3d71a5faded0c9651d66b4
Adham <adhamelsa8er@gmail.com>
2023-12-21 16:07:32 -0800

    a

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,9 +35,9 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_node_t *create_new_node(const char* key, const char* value);
+hash_node_t *create_new_node(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char* key);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 hash_table_t *hash_table_create(unsigned long int size);
